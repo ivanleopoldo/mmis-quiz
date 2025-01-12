cbfbf85fcf9d1d57bb243ec6d20dd5d44e155523
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-12 23:37:31 +0800

    feat(server): grouped routes test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import test from "@/routers/test.router";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.route("/test", test);
 
 export default app;
