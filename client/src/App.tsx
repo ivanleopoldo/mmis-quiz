141e3847891a67c26d6caee23742d049192ed1d0
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-14 01:06:36 +0800

    feat(client): changed login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
+import { GalleryVerticalEnd } from "lucide-react";
+
 import { LoginForm } from "@/components/login-form";
 
-export default function App() {
+export default function LoginPage() {
   return (
-    <div className="min-h-svh flex flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <LoginForm />
+    <div className="min-h-svh grid lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/vite.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Acme Inc.
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
       </div>
     </div>
   );
