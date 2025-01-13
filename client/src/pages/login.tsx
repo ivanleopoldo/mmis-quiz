48079afa2d98810aa3c66da158ad7c90f99dec80
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-14 05:31:39 +0800

    feat(client): sets pointer events to none

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,19 +7,18 @@ export default function Login() {
         <img
           src="/login-left.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="pointer-events-none absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
       <div className="flex flex-col gap-4 bg-[url('/login-bg.svg')] bg-cover bg-center bg-no-repeat p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-28 items-center justify-center rounded-md">
-              <img
-                src="https://cdn.prod.website-files.com/6003cddbd7fb71cb58f75e43/6003d75f4362233400ba87f8_MMIS%20Logo%20(Full).svg"
-                alt="Image"
-              />
-            </div>
-          </a>
+          <div className="flex h-6 w-28 items-center justify-center rounded-md">
+            <img
+              src="https://cdn.prod.website-files.com/6003cddbd7fb71cb58f75e43/6003d75f4362233400ba87f8_MMIS%20Logo%20(Full).svg"
+              alt="Image"
+              className="pointer-events-none"
+            />
+          </div>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
